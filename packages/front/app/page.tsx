f5998f3e67c6caf5c5d18a4b9e591c44e9d9f166
Renzo Sevilha <renzo.sevilha@rtbhouse.com>
2023-06-20 17:04:55 -0300

    (feat/front) style main title

--- packages/front/app/page.tsx
+++ packages/front/app/page.tsx
@@ -1,5 +1,13 @@
 import TabsMenu from './tabs';
 
 export default function Home() {
-  return <TabsMenu />;
+  return (
+    <section>
+      <h1 className="text-center text-4xl font-black text-ctp-lavender">
+        RPGT
+      </h1>
+
+      <TabsMenu />
+    </section>
+  );
 }
