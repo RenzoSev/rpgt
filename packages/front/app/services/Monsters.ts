dffdec35a808714fabff86ab31f3cb1c5ef155e3
Renzo Sevilha <sevilharenzo@gmail.com>
2023-06-21 20:54:49 -0300

    (feat/front) add monsters-service

---
+++ packages/front/app/services/Monsters.ts
@@ -0,0 +1,27 @@
+export interface StatusMonster {
+  attack: number;
+  health: number;
+}
+
+export interface Monster {
+  id: string;
+  name: string;
+  status: StatusMonster;
+}
+
+export class Monsters {
+  async getMonsters(): Promise<Monster[]> {
+    const spider: Monster = {
+      id: 'ads298209382902',
+      name: 'spider',
+      status: {
+        attack: 5,
+        health: 10,
+      },
+    };
+
+    console.log('Starting request for monsters');
+
+    return [spider];
+  }
+}
