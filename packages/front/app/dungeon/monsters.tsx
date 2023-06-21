0aa9099718aaadfc943a9938c4758780404941fa
Renzo Sevilha <sevilharenzo@gmail.com>
2023-06-21 20:55:33 -0300

    (feat/front) create monsters content

---
+++ packages/front/app/dungeon/monsters.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { GiBroadsword, GiTemplarShield } from 'react-icons/gi';
+import { monsters as monstersAtom } from '../store/useMonsters';
+import { useAtom } from 'jotai';
+import { useEffect } from 'react';
+import { Monsters as MonstersService } from '../services/Monsters';
+import { catppuccin } from '../styles/colors';
+
+export default function Monsters() {
+  const [monsters, setMonsters] = useAtom(monstersAtom);
+
+  useEffect(() => {
+    if (monsters.length) return;
+
+    const fetchMonsters = async () => {
+      const monstersService = new MonstersService();
+      const monstersData = await monstersService.getMonsters();
+
+      setMonsters(monstersData);
+    };
+
+    fetchMonsters();
+  }, [monsters.length, setMonsters]);
+
+  return (
+    <>
+      {monsters.map(({ id, name, status: { attack, health } }) => (
+        <div
+          key={id}
+          className="cursor-pointer bg-ctp-base ctp-macchiato border border-ctp-lavender rounded-lg shadow-lg flex justify-between p-3"
+        >
+          <div className="flex flex-col items-start">
+            <p className="text-xl text-ctp-subtext1 font-bold capitalize">
+              {name}
+            </p>
+
+            <div className="flex gap-4">
+              <div className="flex items-center gap-1">
+                <p className="text-lg font-bold text-ctp-subtext0">{attack}</p>
+                <GiBroadsword color={catppuccin.mocha.red} />
+              </div>
+
+              <div className="flex items-center gap-1">
+                <p className="text-lg font-bold text-ctp-subtext0">{health}</p>
+                <GiTemplarShield color={catppuccin.mocha.green} />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <p className="text-lg text-ctp-subtext0 font-bold">1</p>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
--- packages/front/app/dungeon/tab-content.tsx
+++ packages/front/app/dungeon/tab-content.tsx
@@ -1,6 +1,6 @@
-export default function DungeonTabContent() {
-  const renderMonsters = () => {};
+import Monsters from './monsters';
 
+export default function DungeonTabContent() {
   return (
     <>
       <h2 className="text-2xl font-black text-ctp-lavender tracking-wider">
@@ -8,20 +8,7 @@ export default function DungeonTabContent() {
       </h2>
 
       <section>
-        <div className="bg-ctp-base ctp-macchiato border border-ctp-lavender rounded-lg shadow-lg flex justify-between px-2 py-1">
-          <div className="flex flex-col">
-            <p className="text-xl text-ctp-subtext1 font-bold">Spider</p>
-
-            <div className="flex gap-4">
-              <p className="text-lg text-ctp-red font-bold">5</p>
-              <p className="text-lg text-ctp-green font-bold">10</p>
-            </div>
-          </div>
-
-          <div className="flex flex-col justify-center">
-            <p className="text-lg text-ctp-subtext0 font-bold">1</p>
-          </div>
-        </div>
+        <Monsters />
       </section>
     </>
   );
