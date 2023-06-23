76a09cf85d57a431dafc1e260ae4bb943e415ed7
Renzo Sevilha <sevilharenzo@gmail.com>
2023-06-22 22:19:07 -0300

    (feat/front) add tab-title component

---
+++ packages/front/app/components/tab-title.tsx
@@ -0,0 +1,7 @@
+export default function TabTitle({ title }: { title: string }) {
+  return (
+    <h2 className="text-2xl font-black text-ctp-lavender tracking-wider">
+      {title}
+    </h2>
+  );
+}
