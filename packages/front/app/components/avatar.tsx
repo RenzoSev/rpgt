6c432a0df7931284278b40e364aaa79e1560d8c4
Renzo Sevilha <sevilharenzo@gmail.com>
2023-07-12 20:41:04 -0300

    (feat/front) add avatar component

---
+++ packages/front/app/components/avatar.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React from 'react';
+import * as RadixAvatar from '@radix-ui/react-avatar';
+
+interface IAvatar {
+  src: string;
+}
+
+export function Avatar({ src }: IAvatar) {
+  return (
+    <RadixAvatar.Root className="bg-blackA3 inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
+      <RadixAvatar.Image
+        className="h-8 w-8 rounded-[inherit] object-cover"
+        src={src}
+        alt="My profile picture which redirect for my GitHub"
+      />
+      <RadixAvatar.Fallback
+        className="text-violet11 leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium"
+        delayMs={600}
+      >
+        Renzo Sevilha
+      </RadixAvatar.Fallback>
+    </RadixAvatar.Root>
+  );
+}
