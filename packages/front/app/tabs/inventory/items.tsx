22284071e37d3898944f35449dfd6a2c08b8de10
Renzo Sevilha <sevilharenzo@gmail.com>
2023-07-11 21:32:41 -0300

    (feat/front) add alert-dilog for items

--- packages/front/app/tabs/inventory/items.tsx
+++ packages/front/app/tabs/inventory/items.tsx
@@ -6,35 +6,58 @@ import { useService } from '@/app/hooks/useService';
 import classNames from 'classnames';
 import { TabCardName } from '@/app/components/tabs/tab-card-name';
 import { TabCardLevel } from '@/app/components/tabs/tab-card-level';
-import { TabCardStatusItem } from '@/app/components/tabs/tab-card-status-item';
+import {
+  ITabCardStatusItem,
+  TabCardStatusItem,
+} from '@/app/components/tabs/tab-card-status-item';
 import { TabCard } from '@/app/components/tabs/tab-card';
+import { AlertDialog, IAlertDialogTexts } from '@/app/components/alert-dialog';
 
 export default function Items() {
   const itemsService = new ItemsService();
   const { atom: items } = useService(itemsService, itemsAtom);
 
+  const getTexts = (
+    itemName: string,
+    status: ITabCardStatusItem['status']
+  ): IAlertDialogTexts => ({
+    cancelActionMessage: 'Cancel',
+    confirmActionMessage: 'Equip',
+    titleMessage: itemName,
+    descriptionMessage: (
+      <span className="flex gap-4">
+        <TabCardStatusItem status={status} />
+        <TabCardLevel level={status.level} />
+      </span>
+    ),
+  });
+
   return (
     <>
       {items.map(({ id, name, status }) => (
-        <TabCard key={id}>
-          <div className="flex flex-col items-start">
-            <TabCardName name={name} />
+        <AlertDialog key={id} texts={getTexts(name, status)}>
+          <TabCard>
+            <div className="flex flex-col items-start">
+              <TabCardName name={name} />
 
-            <TabCardStatusItem status={status}>
-              <div className="flex items-center gap-1">
-                <span
-                  className={classNames('h-3 w-3 rounded-full', {
-                    'bg-ctp-red': status.equipped,
-                    'bg-ctp-green': !status.equipped,
-                  })}
-                ></span>
-                <p className="text-lg font-bold text-ctp-subtext0">Equipped</p>
-              </div>
-            </TabCardStatusItem>
-          </div>
+              <TabCardStatusItem status={status}>
+                <div className="flex items-center gap-1">
+                  <span
+                    className={classNames('h-3 w-3 rounded-full', {
+                      'bg-ctp-red': status.equipped,
+                      'bg-ctp-green': !status.equipped,
+                    })}
+                  ></span>
+                  <p className="text-lg font-bold text-ctp-subtext0">
+                    Equipped
+                  </p>
+                </div>
+              </TabCardStatusItem>
+            </div>
 
-          <TabCardLevel level={status.level} />
-        </TabCard>
+            <TabCardLevel level={status.level} />
+          </TabCard>
+        </AlertDialog>
       ))}
     </>
   );
