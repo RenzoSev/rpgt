5cf3e5317ba43c168e60ab84070dad9a23136dba
Renzo Sevilha <sevilharenzo@gmail.com>
2023-07-12 21:21:34 -0300

    (feat/front) add dialog component in home-page for play action

--- packages/front/app/page.tsx
+++ packages/front/app/page.tsx
@@ -1,4 +1,5 @@
 import { Avatar } from './components/avatar';
+import { Dialog } from './components/dialog';
 import { AVATAR_PICTURE } from './utils/constants';
 
 export default function Home() {
@@ -9,9 +10,44 @@ export default function Home() {
           RPGT
         </h1>
 
-        <button className="text-ctp-crust bg-ctp-green hover:opacity-60 focus:shadow-ctp-lavender inline-flex h-14 items-center justify-center rounded-[4px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
-          PLAY
-        </button>
+        <Dialog
+          texts={{
+            textShowDialog: 'PLAY',
+            textTitleDialog: 'Account',
+            textDescriptionDialog: 'Login or Sign up',
+            textConfirmAction: 'Create account',
+            secondTextConfirmAction: 'Login',
+          }}
+        >
+          <fieldset className="mb-[15px] flex items-center gap-5">
+            <label
+              className="text-ctp-mauve w-[90px] text-right text-[15px]"
+              htmlFor="name"
+            >
+              Name
+            </label>
+
+            <input
+              className="text-ctp-crust shadow-ctp-lavender focus:shadow-ctp-lavender inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+              id="name"
+              defaultValue="Leeroy Jenkins"
+            />
+          </fieldset>
+
+          <fieldset className="mb-[15px] flex items-center gap-5">
+            <label
+              className="text-ctp-lavender w-[90px] text-right text-[15px]"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="text-ctp-lavender shadow-ctp-lavender focus:shadow-ctp-lavender inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+              id="password"
+              defaultValue="@ljenkins"
+            />
+          </fieldset>
+        </Dialog>
 
         <div className="flex justify-center">
           <p className="text-ctp-lavender text-lg font-bold">
