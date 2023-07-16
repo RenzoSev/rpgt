bd2327a0706382be80456a5a8d45fcd98e26ad1d
Renzo Sevilha <sevilharenzo@gmail.com>
2023-07-16 17:39:22 -0300

    (feat/front) add styles

--- packages/front/app/styles/classes.ts
+++ packages/front/app/styles/classes.ts
@@ -1,1 +1,5 @@
-export const header = 'ctp-macchiato bg-ctp-base flex justify-around p-2 rounded-t';
+export const header =
+  'ctp-macchiato bg-ctp-base flex justify-around p-2 rounded-t';
+
+export const containerContent =
+  'ctp-frappe bg-ctp-base rounded-b h-full text-center p-4';
--- packages/front/app/styles/colors.ts
+++ packages/front/app/styles/colors.ts
@@ -4,5 +4,6 @@ export const catppuccin = {
     green: '#a6e3a1',
     yellow: '#f9e2af',
     mauve: '#cba6f7',
+    peach: '#fab387',
   },
 };
