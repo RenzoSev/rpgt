6a8c4f9acc43fc90f5b6880b20928d766db8a631
Renzo Sevilha <sevilharenzo@gmail.com>
2023-07-18 10:16:00 -0300

    (feat/front) add prefetch to back-to-tabs

--- packages/front/app/battle/[...id]/back-to-tabs.tsx
+++ packages/front/app/battle/[...id]/back-to-tabs.tsx
@@ -6,7 +6,7 @@ import { AiOutlineRollback } from 'react-icons/ai';
 export default function BackToTabs() {
   return (
     <div className="h-[24px] w-[24px]">
-      <Link href={'/tabs'} className="w-full h-full">
+      <Link href={'/tabs'} className="w-full h-full" prefetch={false}>
         <AiOutlineRollback
           className="cursor-pointer"
           color={catppuccin.mocha.mauve}
