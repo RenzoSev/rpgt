9b7f914ad86d1ef3c4197f220535365800d79507
Renzo Sevilha <renzo.sevilha@rtbhouse.com>
2023-07-18 15:48:16 -0300

    add todo

--- packages/front/app/styles/scrollable.ts
+++ packages/front/app/styles/scrollable.ts
@@ -1,1 +1,4 @@
+// TODO: use ScrollArea from Radix
+// https://www.radix-ui.com/docs/primitives/components/scroll-area
+
 export const scrollable = () => 'overflow-y-scroll min-h-[calc(100vh-15rem)]';
