2c48845e1f6c97eefa43929bfe8cf56d1caac98e
Renzo Sevilha <sevilharenzo@gmail.com>
2023-08-23 22:42:50 -0300

    (feat/server) add get and create routes for player

---
+++ packages/server/src/players/player.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PlayerService } from './player.service';
+import { GetPlayerDto } from './dto/get-player.dto';
+import { Player } from './player.schema';
+import { CreatePlayerDto } from './dto/create-player.dto';
+
+@Controller()
+export class PlayerController {
+  constructor(private readonly playerService: PlayerService) { }
+
+  @Get()
+  async get(@Body() getPlayerDto: GetPlayerDto): Promise<Player> {
+    const player = await this.playerService.get(getPlayerDto);
+    return player;
+  }
+
+  @Post()
+  async create(@Body() createPlayerDto: CreatePlayerDto) {
+    const player = await this.playerService.create(createPlayerDto);
+    return player;
+  }
+}
---
+++ packages/server/src/players/player.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Player } from './player.schema';
+import { Model } from 'mongoose';
+import { GetPlayerDto } from './dto/get-player.dto';
+import { CreatePlayerDto } from './dto/create-player.dto';
+
+@Injectable()
+export class PlayerService {
+  constructor(@InjectModel(Player.name) private playerModel: Model<Player>) { }
+
+  async get(getPlayerDto: GetPlayerDto): Promise<Player> {
+    const player = await this.playerModel.findOne(getPlayerDto).exec();
+    return player;
+  }
+
+  async create(createPlayerDto: CreatePlayerDto): Promise<Player> {
+    const player = await this.playerModel.create(createPlayerDto);
+    return player;
+  }
+}
