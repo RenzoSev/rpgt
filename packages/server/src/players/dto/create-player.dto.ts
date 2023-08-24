3114d8caa96001678083f8b1daad0953e9c9a21a
Renzo Sevilha <renzo.sevilha@rtbhouse.com>
2023-08-24 14:37:36 -0300

    (feat/server) add validation

--- packages/server/src/players/dto/create-player.dto.ts
+++ packages/server/src/players/dto/create-player.dto.ts
@@ -1,9 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreatePlayerDto {
+  @IsNotEmpty()
   @IsString()
   readonly name: string;
 
+  @IsNotEmpty()
   @IsString()
   readonly class: string;
 }
