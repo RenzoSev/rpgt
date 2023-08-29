d276bf4100eab59e0038b7a4cec813713364638c
Renzo Sevilha <sevilharenzo@gmail.com>
2023-08-28 21:52:58 -0300

    (fix/server) add missing imports for monsters and players work

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlayerModule } from './players/player.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ItemModule } from './items/item.module';
+import { MonsterModule } from './monsters/monster.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/mydb'),
     PlayerModule,
+    ItemModule,
+    MonsterModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- packages/server/src/monsters/monster.module.ts
+++ packages/server/src/monsters/monster.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Monster, MonsterSchema } from './monster.schema';
+import { MonsterController } from './monster.controller';
+import { MonsterService } from './monster.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Monster.name, schema: MonsterSchema }]),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [MonsterController],
+  providers: [MonsterService],
 })
 export class MonsterModule {}
