45f7e78280ac4da46a394e4ca8912a4dbe4e529f
Renzo Sevilha <sevilharenzo@gmail.com>
2023-09-03 12:06:07 -0300

    (feat/server) add playFightMonster analyze

--- packages/server/src/players/player.analyzer.ts
+++ packages/server/src/players/player.analyzer.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { Item } from '../items/item.schema';
 import { Player } from './player.schema';
-import { ERRORS } from './utils/analyze';
+import { ERRORS, turnsToKill, whoShouldWin } from './utils/analyze';
+import { Monster } from 'src/monsters/monster.schema';
+
+interface Checker {
+  condition: boolean;
+  error: `${ERRORS}`;
+}
+
+type Checkers = Record<string, Checker>;
 
 @Injectable()
 export class PlayerAnalyzer {
   constructor() {}
 
+  private check(checkers: Checkers) {
+    return Object.values(checkers).flatMap(({ condition, error }) =>
+      condition ? error : [],
+    );
+  }
+
   playerBuyItem(player: Player, item: Item) {
-    const checkers: Record<string, { condition: boolean; error: string }> = {
+    const checkers: Checkers = {
       level: {
         condition: player.status.level < item.level,
         error: ERRORS.NOT_ENOUGH_LEVEL,
@@ -19,12 +33,39 @@ export class PlayerAnalyzer {
         error: ERRORS.NOT_ENOUGH_GOLD,
       },
     };
-    const checkersValues = Object.values(checkers);
+    const errors = this.check(checkers);
+    return errors;
+  }
 
-    const errors = checkersValues.flatMap(({ condition, error }) =>
-      condition ? error : [],
+  playerFightMonster(player: Player, monster: Monster) {
+    const playerTurnsToKill = turnsToKill(
+      monster.status.defense,
+      player.status.attack,
     );
+    const monsterTurnsToKill = turnsToKill(
+      player.status.defense,
+      monster.status.attack,
+    );
+    const winner = whoShouldWin(playerTurnsToKill, monsterTurnsToKill);
+    console.log({
+      winner,
+      playerTurnsToKill,
+      monsterTurnsToKill,
+      monsterAttack: monster.status.attack,
+    });
 
+    const checkers = {
+      level: {
+        condition: player.status.level < monster.status.level,
+        error: ERRORS.NOT_ENOUGH_LEVEL,
+      },
+
+      fight: {
+        condition: winner === 'monster',
+        error: ERRORS.NOT_WIN_FIGHT,
+      },
+    };
+    const errors = this.check(checkers);
     return errors;
   }
 }
--- packages/server/src/players/utils/analyze.ts
+++ packages/server/src/players/utils/analyze.ts
@@ -1,4 +1,25 @@
 export enum ERRORS {
   NOT_ENOUGH_GOLD = 'NOT_ENOUGH_GOLD',
   NOT_ENOUGH_LEVEL = 'NOT_ENOUGH_LEVEL',
+  NOT_WIN_FIGHT = 'NOT_WIN_FIGHT',
 }
+
+export enum FIGHTERS {
+  PLAYER = 'player',
+  MONSTER = 'monster',
+}
+
+export type Fighter = `${FIGHTERS}`;
+
+export const turnsToKill = (defense: number, attack: number) =>
+  defense / attack;
+
+export const whoShouldWin = (
+  playerTurnsToKill: number,
+  monsterTurnsToKill: number,
+): Fighter => {
+  if (monsterTurnsToKill < 1 && playerTurnsToKill < 1) return FIGHTERS.PLAYER;
+
+  const result = playerTurnsToKill < monsterTurnsToKill;
+  return result ? FIGHTERS.PLAYER : FIGHTERS.MONSTER;
+};
